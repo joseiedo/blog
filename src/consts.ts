ed0f4186528f5e2e283ea0fdac47689301289752
iedo <iedoduarte1@gmail.com>
2025-11-22 22:01:10 -0300

    feat: add dynamic programming intro

--- src/consts.ts
+++ src/consts.ts
@@ -16,14 +16,6 @@ export const NAV_LINKS: SocialLink[] = [
     href: '/blog',
     label: 'blog',
   },
-  // {
-  //   href: '/authors',
-  //   label: 'authors',
-  // },
-  // {
-  //   href: '/about',
-  //   label: 'about',
-  // },
 ]
 
 export const SOCIAL_LINKS: SocialLink[] = [
